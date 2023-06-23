ee7e1cabe7375809e4d7845e7f72a9ca7d329359
Arina <rimozkovaya@gmail.com>
2023-06-23 23:21:07 +0300

    popular&categories

--- src/js/app.js
+++ src/js/app.js
@@ -28,3 +28,31 @@ const swiper = new Swiper('.auctions-slider', {
 	freeMode: true,
 	grabCursor: true,
 });
+
+const swiper2 = new Swiper('.popular-slider', {
+	navigation: {
+		nextEl: '.swiper-button-next_2',
+		prevEl: '.swiper-button-prev_2' // Включить стрелочки
+	},
+	modules: [Navigation, FreeMode], // Модули, которые будут использоваться
+	autoHeight: true, // Автовысота
+	speed: 500, // Скорость прокрутки слайдера
+	slidesPerView: 2.5, // Количество слайдов, которые будут видны
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true,
+});
+
+const swiper3 = new Swiper('.categories-slider', {
+	navigation: {
+		nextEl: '.swiper-button-next_3',
+		prevEl: '.swiper-button-prev_3' // Включить стрелочки
+	},
+	modules: [Navigation, FreeMode], // Модули, которые будут использоваться
+	autoHeight: true, // Автовысота
+	speed: 500, // Скорость прокрутки слайдера
+	slidesPerView: 2.5, // Количество слайдов, которые будут видны
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true,
+});
